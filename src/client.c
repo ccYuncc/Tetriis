02babe1502444ae6c6c114c76dbba3100402db64
Tomas-Duthoit <duthoit.thomas9@gmail.com>
2025-11-28 17:13:45 +0100

    basic files

---
+++ src/client.c
@@ -0,0 +1,20 @@
+/**
+ *      _____ ___ _____ ___ ___ ___ ___
+ *     |_   _| __|_   _| _ \_ _|_ _/ __|
+ *       | | | _|  | | |   /| | | |\__ \
+ *       |_| |___| |_| |_|_\___|___|___/
+ *
+ * Project : Tetriis
+ * File name : client.c
+ * Description :  
+ *          Main file for the client.
+ *          Allows connexion to the server to play in multiplayer mode
+ * Authors : DUTHOIT Thomas / GREBERT Cloé
+ */
+
+#include "shared.h" 
+
+int main(){
+
+    return 0;
+}
